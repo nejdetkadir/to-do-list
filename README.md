44ceed506f6ee356fc4c1ba7277bcc05cdc9469e
nejdetkadir <nejdetkadir.550@gmail.com>
2020-08-26 22:23:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# TO-DO LIST
+
+This is a simple to-do app. If you want to use it, you can try [demo](https://demo.nejdetkadirbektas.com/to-do-list/) of to-do application.
+
+# Screenshot
+![screenshot](doc/ss.png)
---
+++ dist/App.dev.js
@@ -0,0 +1,136 @@
+"use strict";
+
+var todoController = function () {
+  var Todo = function Todo(id, description) {
+    this.id = id;
+    this.description = description;
+    this.time = new Date().getFullYear() + '/' + new Date().getMonth() + '/' + new Date().getDay();
+  };
+
+  var data = {
+    allItems: []
+  };
+  return {
+    addItem: function addItem(desc) {
+      var newItem, ID; // Create new ID
+
+      if (data.allItems.length > 0) {
+        ID = data.allItems[data.allItems.length - 1].id + 1;
+      } else {
+        ID = 0;
+      } // Create new item
+
+
+      newItem = new Todo(ID, desc); // Push it into our data structure
+
+      data.allItems.push(newItem); // Return the new element
+
+      return newItem;
+    },
+    deleteItem: function deleteItem(id) {
+      var ids, index;
+      ids = data.allItems.map(function (curr) {
+        return curr.id;
+      });
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems.splice(index, 1);
+      }
+    },
+    testing: function testing() {
+      console.log(data);
+    }
+  };
+}();
+
+var UIController = function () {
+  var DOMStrings = {
+    inputField: '.action_input',
+    todoList: '.willList',
+    addBut: '.add-button',
+    container: '.main',
+    delBut: 'deleteButton'
+  };
+  return {
+    getInput: function getInput() {
+      return document.querySelector(DOMStrings.inputField).value;
+    },
+    addListItem: function addListItem(obj) {
+      var html, newHtml, element;
+      element = DOMStrings.todoList;
+      html = '<div class="item" id="will-%id%"><div class="right floated content"><button class="ui inverted red button deleteButton"><i class="trash alternate icon"></i></button></div><div class="content"><div class="header todo">%description% (%time%)</div></div></div>'; // Replace the placeholder text with some actual data
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%time%', obj.time); // Insert the HTML into the DOM
+
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+    deleteListItem: function deleteListItem(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
+    },
+    clearField: function clearField() {
+      var field;
+      field = document.querySelector(DOMStrings.inputField);
+      field.value = "";
+      field.focus();
+    },
+    getDOMStrings: function getDOMStrings() {
+      return DOMStrings;
+    }
+  };
+}();
+
+var controller = function (todoCtrl, UICtrl) {
+  var setupEventListeners = function setupEventListeners() {
+    var DOM = UICtrl.getDOMStrings();
+    document.querySelector(DOM.addBut).addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+  };
+
+  var ctrlDeleteItem = function ctrlDeleteItem(event) {
+    var itemID, splitID;
+
+    if (event.path[0].parentNode.classList[4] === UICtrl.getDOMStrings.delBut) {
+      itemID = event.target.parentNode.parentNode.id;
+    } else if (event.path[0].classList[4] === UICtrl.getDOMStrings.delBut) {
+      itemID = event.target.parentNode.parentNode.parentNode.id;
+    }
+
+    if (itemID) {
+      splitID = itemID.split('-');
+      type = splitID[0];
+      ID = parseInt(splitID[1]); // 1. Delete the item from the data structure
+
+      todoCtrl.deleteItem(ID); // 2. Delete the item from the UI
+
+      UICtrl.deleteListItem(itemID);
+    }
+  };
+
+  var ctrlAddItem = function ctrlAddItem() {
+    var input, newItem; // 1. Get the field input data
+
+    input = UICtrl.getInput();
+
+    if (input !== "") {
+      // 2. Add the item to the to do controller
+      newItem = todoCtrl.addItem(input); // 3. Add the item to the UI
+
+      UICtrl.addListItem(newItem); // 4. Clear the fields
+
+      UICtrl.clearField();
+    }
+  };
+
+  return {
+    init: function init() {
+      console.log('Application has started.');
+      setupEventListeners();
+    }
+  };
+}(todoController, UIController);
+
+controller.init();
